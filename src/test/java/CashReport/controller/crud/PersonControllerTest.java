df90842654653ec81ac6c540d4eb98d5288b5539
xarczuz <chjunchi.chiu@gmail.com>
2020-02-27 16:39:15 +0100

    First Test method done

--- src/test/java/CashReport/controller/crud/PersonControllerTest.java
+++ src/test/java/CashReport/controller/crud/PersonControllerTest.java
@@ -2,14 +2,17 @@ package CashReport.controller.crud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-import java.io.UnsupportedEncodingException;
-
-import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,34 +23,29 @@ class PersonControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void name() throws UnsupportedEncodingException {
-
-//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//        params.add("grant_type", "password");
-//        params.add("client_id", "fooClientIdPassword");
-//        params.add("username", username);
-//        params.add("password", password);
-//
-//        ResultActions result
-//                = mockMvc.perform(post("/oauth/token")
-//                .params(params)
-//                .with(httpBasic("fooClientIdPassword","secret"))
-//                .accept("application/json;charset=UTF-8"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType("application/json;charset=UTF-8"));
-//
-//        String resultString = result.andReturn().getResponse().getContentAsString();
-//
-//        JacksonJsonParser jsonParser = new JacksonJsonParser();
-//        jsonParser.parseMap(resultString).get("access_token").toString();
+    private String obtainAccessToken(String username, String password) throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client_id", "perham");
+        params.add("username", username);
+        params.add("password", password);
+        ResultActions result
+                = mockMvc.perform(post("/oauth/token")
+                .params(params)
+                .with(httpBasic("perham", "1234"))
+                .accept("application/json;charset=UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+        String resultString = result.andReturn().getResponse().getContentAsString();
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resultString).get("access_token").toString();
     }
 
     @Test
-    @WithMockUser
     void getAllPerson() throws Exception {
-        this.mockMvc.perform(get("/person")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, World")));
+        String accessToken = obtainAccessToken("Ankan", "1234");
+        this.mockMvc.perform(get("/person").header("Authorization", "Bearer " + accessToken))
+                .andDo(print()).andExpect(status().isOk());
     }
 
     @Test
