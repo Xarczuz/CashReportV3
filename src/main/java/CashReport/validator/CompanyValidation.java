c85c7a414b1ac78eefcc1e9246acba4e3699288a
Tobias Mindell <tobiasmindell@gmail.com>
2020-02-27 16:18:36 +0100

    validation Company and Person

---
+++ src/main/java/CashReport/validator/CompanyValidation.java
@@ -0,0 +1,44 @@
+package CashReport.validator;
+
+import CashReport.model.Company;
+import java.util.function.Function;
+import static CashReport.validator.CompanyValidation.CompanyEnum;
+import static CashReport.validator.CompanyValidation.CompanyEnum.*;
+
+
+public interface CompanyValidation extends Function<Company,CompanyEnum> {
+
+
+
+   default CompanyValidation checkOrgNrValidator(){
+       return company -> company.getOrgnr() > 0 ? SUCCESS : ORGANISATION_NUMBER_NOT_VALID;
+   };
+
+   default CompanyValidation companyNameValidator(){
+       return company -> company.getCompany().length() > 50 ? SUCCESS : COMPANY_NAME_NOT_VALID;
+   }
+
+   default CompanyValidation streetNameValidator(){
+       return company -> {
+           return company.getStreet().matches("[a-zA-Z].+") ? SUCCESS: STREET_NOT_VALID; //Regex check if first char is a letter
+       };
+   }
+
+   default CompanyValidation cityValidation(){
+       return company -> company.getCity().matches("[a-zA-Z]") ? SUCCESS : CITY_NOT_VALID;
+   }
+
+   default CompanyValidation zipcodeValidation(){
+       return company -> company.getZipcode().matches("[0-9 ]+") ? SUCCESS : ZIP_CODE_NOT_VALID;
+   }
+
+
+    enum CompanyEnum {
+        ORGANISATION_NUMBER_NOT_VALID,
+        COMPANY_NAME_NOT_VALID,
+        STREET_NOT_VALID,
+        CITY_NOT_VALID,
+        ZIP_CODE_NOT_VALID,
+        SUCCESS;
+    }
+}
---
+++ src/main/java/CashReport/validator/PersonValidation.java
@@ -0,0 +1,46 @@
+package CashReport.validator;
+
+import CashReport.model.Person;
+
+import java.util.function.Function;
+
+import static CashReport.validator.PersonValidation.PersonEnum;
+import static CashReport.validator.PersonValidation.PersonEnum.*;
+
+public interface PersonValidation extends Function<Person, PersonEnum> {
+
+
+    default PersonValidation nameValidator() {
+        return person -> person.getFirstname().matches("[a-zA-]") ? SUCCESS : NAME_NOT_VALID;
+    };
+
+    default PersonValidation lastNameValidator() {
+        return person -> person.getLastname().matches("[a-zA-]") ? SUCCESS : LASTNAME_NOT_VALID;
+    };
+
+    default PersonValidation phoneNumberValidator() {
+        return person -> person.getPhonenr().matches("[0-9]*") ? SUCCESS : NUMBER_NOT_VALID;
+    };
+
+    default PersonValidation usernameValidator() {
+        return person -> person.getUsername().length() > 50 ? SUCCESS : USERNAME_NOT_VALID;
+    };
+
+    default PersonValidation emailValidator() {
+        return person -> person.getEmail().contains("@") ? SUCCESS : EMAIL_NOT_VALID;
+    };
+
+    enum PersonEnum {
+        NAME_NOT_VALID,
+        EMAIL_NOT_VALID,
+        LASTNAME_NOT_VALID,
+        NUMBER_NOT_VALID,
+        SUCCESS,
+        USERNAME_NOT_VALID,
+        PASSWORD_NOT_VALID,
+        SALT_NOT_VALID,
+        ROLE_NOT_VALID,
+        COMPANY_NOT_VALID;
+    }
+
+}
